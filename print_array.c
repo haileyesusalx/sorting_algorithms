c4cad54bd796ba15c6c887544ebbea74d1eaa559
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-04-25 14:01:47 -0700

    task 1

---
+++ print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * print_array - prints an array of integers
+ * @array: pointer to the array to print
+ * @size: size of the array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    for (i = 0; i < size; i++)
+    {
+        printf("%d", array[i]);
+        if (i != size - 1)
+            printf(", ");
+    }
+    printf("\n");
+}
