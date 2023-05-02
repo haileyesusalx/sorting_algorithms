c77184c3af1d245ef98fd5c43fa8df751d2d0730
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-05-01 17:52:06 -0700

    task 0

---
+++ sort.h
@@ -0,0 +1,13 @@
+#ifndef SORT_H
+#define SORT_H
+
+/* function prototype for printing an array of integers */
+void print_array(const int *array, size_t size);
+
+/* function prototype for swapping two integers */
+void swap(int *a, int *b);
+
+/* function prototype for bubble sort algorithm */
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
