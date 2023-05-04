822c388a5065cbb2458638f11fede7ffe974e5a2
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-05-03 18:13:00 -0700

    task 4

---
+++ 100-shell_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * swap_ints - swaps two integers
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ *
+ * Return: void
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * shell_sort - sorts an array of integers in ascending order using the
+ *                  Selection sort algorithm
+ * @array: pointer to the array of integers
+ * @size: size of the array
+ * Return:The final partition index
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap + 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
